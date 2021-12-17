7ffe76b01bc739953a37005fe985fee4e5c56641
cmj <cmj@cmj.tw>
2021-12-17 09:56:45 +0800

    feat(stropt): check the pass value
    	only allow pass *Struct, otherwire return error

--- stropt.go
+++ stropt.go
@@ -1,9 +1,28 @@
 package stropt
 
+import (
+	"fmt"
+	"reflect"
+)
+
 type StrOpt struct {
+	reflect.Value
 }
 
 func New(in interface{}) (stropt *StrOpt, err error) {
+	value := reflect.ValueOf(in)
+	value.Kind()
+
+	// only allow pass the *Struct
+	if !(value.Kind() == reflect.Ptr && value.Elem().Kind() == reflect.Struct) {
+		err = fmt.Errorf("should pass *Struct: %T", in)
+		return
+	}
+
+	stropt = &StrOpt{
+		Value: value,
+	}
+
 	return
 }
 
