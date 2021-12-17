4d3a4b4cec949637cfc3d95445e2d88b791dbf90
cmj <cmj@cmj.tw>
2021-12-17 09:57:22 +0800

    test(stropt): add testcase for pass invalid type

--- stropt_test.go
+++ stropt_test.go
@@ -1,9 +1,38 @@
 package stropt
 
+import (
+	"testing"
+)
+
 type Foo struct {
 }
 
 func Example() {
 	foo := Foo{}
 	MustNew(foo)
+}
+
+func TestInvalidType(t *testing.T) {
+	cases := []interface{}{
+		nil,
+		true,
+		false,
+		1,
+		1.2,
+		'x',
+		"test",
+		rune(123),
+	}
+
+	for _, c := range cases {
+		if _, err := New(c); err == nil {
+			t.Errorf("expect cannot pass %T", c)
+			continue
+		}
+
+		if _, err := New(&c); err == nil {
+			t.Errorf("expect cannot pass %T", c)
+			continue
+		}
+	}
 }
