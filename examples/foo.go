ecb9f27396328c9ca7bf375ce32ef7941b08c371
cmj <cmj@cmj.tw>
2021-12-29 16:14:11 +0800

    feat(examples): add the example for parse and show result
    	Add the foo example for parse the flip/flag (built-in type)

--- examples/foo.go
+++ examples/foo.go
@@ -11,6 +11,12 @@ type Foo struct {
 	stropt.Model
 
 	Flip bool `shortcut:"f" desc:"store true/false value"`
+
+	Number  int        `shortcut:"n" desc:"store integer"`
+	Age     uint       `shortcut:"a" desc:"store unsigned integer"`
+	Price   float64    `shortcut:"p" desc:"store float number, may rational number"`
+	Point   complex128 `shortcut:"P" desc:"store the complex as point"`
+	Message string     `shortcut:"m" desc:"store the raw string"`
 }
 
 func main() {
