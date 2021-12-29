3157140954112edc4a481a1045fef485886d15e3
cmj <cmj@cmj.tw>
2021-12-29 23:02:12 +0800

    test(argument): add testcase for argument

--- examples/foo.go
+++ examples/foo.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 
 	"github.com/cmj0121/stropt"
@@ -12,11 +13,13 @@ type Foo struct {
 
 	Flip bool `shortcut:"f" desc:"store true/false value"`
 
-	Number  int        `shortcut:"n" desc:"store integer"`
-	Age     uint       `shortcut:"a" desc:"store unsigned integer"`
-	Price   float64    `shortcut:"p" desc:"store float number, may rational number"`
-	Point   complex128 `shortcut:"P" desc:"store the complex as point"`
-	Message string     `shortcut:"m" desc:"store the raw string"`
+	Number  int     `shortcut:"n" desc:"store integer"`
+	Age     uint    `shortcut:"a" desc:"store unsigned integer"`
+	Price   float64 `shortcut:"p" desc:"store float number, may rational number"`
+	Message string  `shortcut:"m" desc:"store the raw string"`
+
+	Name   *string `desc:"save name as argument"`
+	Amount *int    `desc:"save int as argument"`
 }
 
 func main() {
@@ -25,5 +28,6 @@ func main() {
 	parser.Version("foo (demo)")
 
 	parser.Run()
-	fmt.Printf("%#v\n", foo)
+	text, err := json.MarshalIndent(foo, "", "    ")
+	fmt.Printf("error: %v\n%v\n", err, string(text))
 }
