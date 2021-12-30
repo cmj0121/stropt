570c08c2fe04009f46a998188d1da796f7cabf20
cmj <cmj@cmj.tw>
2021-12-30 18:35:11 +0800

    feat(examples): add examples for the sub-command
    	Parse the sub-command and show the result

--- examples/foo.go
+++ examples/foo.go
@@ -3,9 +3,22 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"os"
 
 	"github.com/cmj0121/stropt"
 )
+
+type Sub struct {
+	stropt.Model
+
+	Age uint `shortcut:"a" desc:"store unsigned integer"`
+}
+
+func (sub Sub) Version_(stropt *stropt.StrOpt, _field stropt.Field) (err error) {
+	os.Stdout.WriteString("sub (demo)")
+	os.Exit(0)
+	return
+}
 
 // the example struct and ready to setup by stropt
 type Foo struct {
@@ -20,14 +33,26 @@ type Foo struct {
 
 	Name   *string `desc:"save name as argument"`
 	Amount *int    `desc:"save int as argument"`
+
+	*Sub `json:"sub" desc:"sub-command"`
 }
 
 func main() {
-	foo := Foo{}
+	name := "defualt-name"
+
+	foo := Foo{
+		Name: &name,
+
+		Sub: &Sub{
+			Age: 123,
+		},
+	}
+
 	parser := stropt.MustNew(&foo)
 	parser.Version("foo (demo)")
 
 	parser.Run()
 	text, err := json.MarshalIndent(foo, "", "    ")
 	fmt.Printf("error: %v\n%v\n", err, string(text))
+	fmt.Printf("\n%#v\n", foo)
 }
