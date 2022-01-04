8b18fe22cd4c8257de232602de4085c2ad93b246
cmj <cmj@cmj.tw>
2022-01-04 09:57:59 +0800

    feat(): bump version to v0.2.0

--- config.go
+++ config.go
@@ -5,7 +5,7 @@ const (
 	PROJ_NAME = "stropt"
 	// the version meta
 	MAJOR = 0 // bump when API changed
-	MINOR = 1 // bump when new feature add
+	MINOR = 2 // bump when new feature add
 	MACRO = 0 // bump when bug fixed
 )
 
