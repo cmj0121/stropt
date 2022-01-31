b3922e9be43548e2f8e4af14c0b80415ca29984b
cmj <cmj@cmj.tw>
2022-01-31 21:13:56 +0800

    bump version to v0.4.0

--- config.go
+++ config.go
@@ -5,8 +5,8 @@ const (
 	PROJ_NAME = "stropt"
 	// the version meta
 	MAJOR = 0 // bump when API changed
-	MINOR = 3 // bump when new feature add
-	MACRO = 5 // bump when bug fixed
+	MINOR = 4 // bump when new feature add
+	MACRO = 0 // bump when bug fixed
 )
 
 // pre-defined key of the tag
